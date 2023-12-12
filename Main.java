655407d22caadde1ce96f5c93d4c7db624a33432
brodyns <118648344+brodyns@users.noreply.github.com>
2023-12-12 16:46:57 +0800

    206. 反转链表

---
+++ Main.java
@@ -0,0 +1,38 @@
+﻿public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+class Solution{
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while(cur != null){
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+    public ListNode reverseList2(ListNode head) {
+        if(head == null || head.next == null){
+            return null;
+        }
+        ListNode newHead = reverseList2(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
